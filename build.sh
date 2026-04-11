bc99a6b24c311e06d8e93420b42b37a124e7d79b
Leo <st128618@student.spbu.ru>
2026-04-11 18:02:49 +0300

    Disable NER download, switch to E5-small in build

--- build.sh
+++ build.sh
@@ -8,34 +8,33 @@ pip install --upgrade pip
 pip install -r backend/requirements.txt
 pip install huggingface-hub
 
-# Предзагрузка sentence-transformers модели (E5-large)
-echo "Предзагрузка E5-large модели..."
+# Предзагрузка E5-small модели (вместо E5-large)
+echo "Предзагрузка E5-small модели..."
 python -c "
 from sentence_transformers import SentenceTransformer
-import sys
-print('Загрузка intfloat/multilingual-e5-large...')
-model = SentenceTransformer('intfloat/multilingual-e5-large')
-print('E5-large модель загружена')
+print('Загрузка intfloat/multilingual-e5-small...')
+model = SentenceTransformer('intfloat/multilingual-e5-small')
+print('E5-small модель загружена')
 "
 
-# Скачивание NER модели с HuggingFace
-echo "Скачивание NER модели..."
-python -c "
-from huggingface_hub import snapshot_download
-import os
-
-model_path = 'ml/nlp/models/rubio_ner_finetuned_74'
-if not os.path.exists(os.path.join(model_path, 'model.safetensors')):
-    print('Скачивание NER модели...')
-    snapshot_download(
-        repo_id='velto006/rubio-ner-finetuned-74',
-        local_dir=model_path,
-        local_dir_use_symlinks=False,
-        ignore_patterns=['*.pt', 'optimizer-*.pt', 'scheduler.pt', 'rng_state.pth', 'trainer_state.json']
-    )
-    print('NER модель загружена')
-else:
-    print('NER модель уже есть в кеше')
-"
+# Скачивание NER модели - ВРЕМЕННО ОТКЛЮЧЕНО
+# echo "Скачивание NER модели..."
+# python -c "
+# from huggingface_hub import snapshot_download
+# import os
+# 
+# model_path = 'ml/nlp/models/rubio_ner_finetuned_74'
+# if not os.path.exists(os.path.join(model_path, 'model.safetensors')):
+#     print('Скачивание NER модели...')
+#     snapshot_download(
+#         repo_id='velto006/rubio-ner-finetuned-74',
+#         local_dir=model_path,
+#         local_dir_use_symlinks=False,
+#         ignore_patterns=['*.pt', 'optimizer-*.pt', 'scheduler.pt', 'rng_state.pth', 'trainer_state.json']
+#     )
+#     print('NER модель загружена')
+# else:
+#     print('NER модель уже есть в кеше')
+# "
 
 echo "Сборка завершена!"
